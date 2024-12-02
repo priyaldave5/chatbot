18dcb492a44ca0158075bcf699eadc556f7a1502
priyaldave5 <priyaldave007@gmail.com>
2024-12-02 15:31:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,6 @@ def main():
 
           
     elif choice == "About":
- st.subheader("Conclusion:")
         st.write("A chatbot is a computer program that simulates and processes human conversation, allowing humans to interact with digital devices as if they were communicating with a real person.")
 
     
