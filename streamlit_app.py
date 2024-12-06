4ffabcad3535b185e0817c56b1787bfd54b5adb5
priyaldave5 <priyaldave007@gmail.com>
2024-12-06 10:47:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,16 +45,15 @@ def main():
     global counter
     st.title("WELCOME! This is a chatbot created by Priyal")
 
-    menu = ["Home", "Conversation History""About"]
+    menu = ["Home","About"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
         st.write("Please type a message and press Enter to start the conversation.")
         
-        user_input = st.text_input("You:", key=f"user_input_{counter}")
+    
   
 
-          
     elif choice == "About":
         st.write("PROBLEM STATEMENT :To built a conversational chatbot that can interact with users, understand their queries and give appropriate response to them. The chatbot should be able to understand NLP, process it and deliver coherent replies using NLP techniques.A chatbot is a computer program that simulates and processes human conversation, allowing humans to interact with digital devices as if they were communicating with a real person.")
     
